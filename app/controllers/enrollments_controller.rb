13d614a9d2e511f638c81c42b9c767e54177622d
Esdras Vidal Pereira <esdras015@gmail.com>
2024-09-13 23:33:03 -0300

    Adding routes and controllers

---
+++ app/controllers/enrollments_controller.rb
@@ -0,0 +1,30 @@
+class EnrollmentsController < ApplicationController
+  def create
+    enrollment = Enrollment.new(enrollment_params)
+    if enrollment.save
+      render json: enrollment, status: :created
+    else
+      render json: enrollment.errors, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    enrollment = Enrollment.find_by(user_id: params[:user_id], team_id: params[:team_id])
+    if enrollment
+      render json: enrollment
+    else
+      render json: { error: 'Enrollment not found' }, status: :not_found
+    end
+  end
+
+  def index
+    enrollments = Enrollment.where(role_id: params[:role_id])
+    render json: enrollments
+  end
+
+  private
+
+  def enrollment_params
+    params.require(:enrollment).permit(:user_id, :team_id, :role_id)
+  end
+end
